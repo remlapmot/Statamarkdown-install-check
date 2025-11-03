76853ab7a6ff15178f22bd6075e2f945a77363dc
Tom Palmer <remlapmot@hotmail.com>
2025-11-03 17:32:23 +0000

    Update install-binary.R

--- install-binary.R
+++ install-binary.R
@@ -12,17 +12,17 @@ if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   # pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install(c("Statamarkdown"), dependencies = TRUE)
+  pak::pkg_install("Statamarkdown", dependencies = TRUE)
 } else if (Sys.info()["machine"] == "x86_64") {
   pak::repo_add(CRAN = "https://p3m.dev/cran/__linux__/noble/latest")
   pak::repo_add(CRANbackup = "https://cloud.r-project.org")
-  pak::pkg_install(c("Statamarkdown"), dependencies = TRUE)
+  pak::pkg_install("Statamarkdown", dependencies = TRUE)
 } else if (Sys.info()["machine"] == "aarch64") {
   # Linux aarch64
   pak::repo_add(CRAN = "https://cran.r-universe.dev/bin/linux/noble-aarch64/4.5")
   pak::repo_add(CRANbackup = "https://cloud.r-project.org")
   # pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install("Hemken/Statamarkdown", dependencies = TRUE)
+  pak::pkg_install("Statamarkdown", dependencies = TRUE)
 }
 
 library(Statamarkdown)
